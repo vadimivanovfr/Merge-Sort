0610ffe40ef7aac159be0e2c96e2041f9b514bf1
Vadim V <vadimivanovfr@gmail.com>
2017-01-26 15:30:12 +0300

    First Commit

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -10,6 +10,49 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter digits:");
+            string input = Console.ReadLine();
+            int[] digits = Array.ConvertAll(input.Split(' '), int.Parse);
+            digits = Sort(digits);
+            foreach (int d in digits)
+                Console.Write(d + " ");
+        }
+
+        static int[] Sort(int[] c)
+        {
+            if (c.Length <= 1)
+                return c;
+            else
+            {
+                int middleNum = c.Length / 2;
+                int rightSize = c.Length - middleNum;
+                int[] left = new int[middleNum];
+                int[] right = new int[rightSize];
+                Array.Copy(c, left, middleNum);
+                Array.Copy(c, middleNum, right, 0, rightSize);
+                left = Sort(left);
+                right = Sort(right);
+                return Merge(left, right);
+            }
+        }
+
+        static int[] Merge(int[] a, int[] b)
+        {
+            int[] c = new int[a.Length + b.Length];
+            for(int i = 0, j = 0, k = 0; k < a.Length + b.Length; k++)
+            {
+                if ((j >= b.Length) || (i < a.Length && a[i] < b[j]))
+                {
+                    c[k] = a[i];
+                    i++;
+                }
+                else
+                {
+                    c[k] = b[j];
+                    j++;
+                }
+            }
+            return c;
         }
     }
 }
