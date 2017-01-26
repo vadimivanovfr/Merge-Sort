ba8623d5541808cc0e504132aca338b9c4459c05
Vadim V <vadimivanovfr@gmail.com>
2017-01-26 22:55:21 +0300

    Added Comments

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MergeSort
 {
@@ -10,32 +6,34 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter digits:");
+            Console.WriteLine("Enter numbers separated by a space:");
             string input = Console.ReadLine();
-            int[] digits = Array.ConvertAll(input.Split(' '), int.Parse);
+            int[] digits = Array.ConvertAll(input.Split(' '), int.Parse); // Splitting entered numbers
             digits = Sort(digits);
             foreach (int d in digits)
                 Console.Write(d + " ");
+            Console.WriteLine();
         }
 
         static int[] Sort(int[] c)
         {
-            if (c.Length <= 1)
+            if (c.Length <= 1) // divide until there is only one element in array
                 return c;
             else
             {
-                int middleNum = c.Length / 2;
-                int rightSize = c.Length - middleNum;
-                int[] left = new int[middleNum];
-                int[] right = new int[rightSize];
-                Array.Copy(c, left, middleNum);
-                Array.Copy(c, middleNum, right, 0, rightSize);
+                int middleNum = c.Length / 2; // Index of the middle element
+                int rightSize = c.Length - middleNum; // Amount of numbers in second half
+                int[] left = new int[middleNum]; // first half of numbers
+                int[] right = new int[rightSize]; // second half of numbers
+                Array.Copy(c, left, middleNum); // Copying first half
+                Array.Copy(c, middleNum, right, 0, rightSize); // Copying second half
                 left = Sort(left);
                 right = Sort(right);
                 return Merge(left, right);
             }
         }
-
+        
+        // comparison of two subarrays and merging them into one
         static int[] Merge(int[] a, int[] b)
         {
             int[] c = new int[a.Length + b.Length];
